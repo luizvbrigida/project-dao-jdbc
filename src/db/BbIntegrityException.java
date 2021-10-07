130bc86f7b9ae86c32af82c31fffe7b8562704c9
Luiz Vinicius Brígida <l.vinicius_@hotmail.com>
2021-10-07 16:20:41 -0300

    create project

---
+++ src/db/BbIntegrityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class BbIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BbIntegrityException(String msg) {
+		super(msg);
+	}
+	
+}
