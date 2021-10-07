415cae9487f0506dfa9bb6551b06e301eb007097
Luiz Vinicius Brígida <l.vinicius_@hotmail.com>
2021-10-07 17:09:45 -0300

    passo2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Book");
+		Seller seller = new Seller(22, "teste", "Email.com", new Date(), 222.0, obj);
 		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
